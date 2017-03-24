7c17fdb4f6c9db05e798eb1003e388db71da9a27
s3428123-Ruaraidh <s3428123@student.rmit.edu.au>
2017-03-24 11:04:01 +1100

    addCustomer() added, but doesn't write to file yet Some work on loadData()

--- BookingManager/src/bookingmanager/Login.java
+++ BookingManager/src/bookingmanager/Login.java
@@ -2,6 +2,7 @@
 package bookingmanager;
 import java.util.ArrayList;
 import java.util.Date;
+import org.json;
 
 public class Login 
 {	
@@ -39,5 +40,41 @@ public class Login
         public void loadTestUsers()
         {
             users.add(new Customer(0, "test@test.com", "test", "Jim", "Jenson", new Date(), "00000000"));
+        }
+        
+        /*
+        public void loadData()
+        {
+        	Customer newUser;
+        	jsonArray = new JSONArray();
+        	for(int i =0; jsonArray.length(); i++)
+        	{
+        		newUser.a_id = (JSONObject) jsonArray.get("id");
+        		newUser.a_email = (JSONObject) jsonArray.get("email");
+        		newUser.a_password = (JSONObject) jsonArray.get("password");
+        		newUser.a_firstName = (JSONObject) jsonArray.get("fname");
+        		newUser.a_lastName = (JSONObject) jsonArray.get("lname");
+        		newUser.a_dob = (JSONObject) jsonArray.get("dob");
+        		newUser.a_number = (JSONObject) jsonArray.get("number");
+        	}
+        	
+        	users.add(newUser);
+        }
+        */
+        public boolean addCustomer(int id, String email, String password, String fname, String lname, Date dob, String number)
+        {
+        	users.add(new Customer(id, email, password, fname, lname, dob, number));
+        	/* Check if user added */
+        	for(int i = 0; i < users.size(); i++)
+        	{
+        		if(id == users.get(i).getId())
+        		{
+        			return true;
+        		}
+        		else
+        		{
+        			return false;
+        		}
+        	}
         }
 }
