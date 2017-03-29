2dd7a3366540e04d42d04ae15ae63724c82941a7
Joshua Coombes-Hall <s3589479@student.rmit.edu.au>
2017-03-29 16:44:38 +1100

    merge

--- BookingManager/src/bookingmanager/Timeslot.java
+++ BookingManager/src/bookingmanager/Timeslot.java
@@ -3,18 +3,12 @@ import java.util.Date;
 
 /* @author Tony Tan - s3439530 */
 public class Timeslot {
-	private int startTime;
 	private Date apptDate;
 	
-	Timeslot(int a_startTime, Date a_apptDate) {
-		startTime = a_startTime;
+	Timeslot(Date a_apptDate) {
 		apptDate = a_apptDate;
 	}
-	
-	public int getStartTime() {
-		return startTime;
-	}
-	
+
 	public Date getDate() {
 		return apptDate;
 	}
