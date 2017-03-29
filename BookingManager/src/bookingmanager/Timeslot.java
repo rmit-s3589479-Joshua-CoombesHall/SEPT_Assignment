7d574543a96157fc0d2c2680ed9cd93819abd0f1
udonge <lolicreamed@gmail.com>
2017-03-29 15:49:43 +1100

    Update Timeslot.java
    int startTime change to Date type.

--- BookingManager/src/bookingmanager/Timeslot.java
+++ BookingManager/src/bookingmanager/Timeslot.java
@@ -3,15 +3,15 @@ import java.util.Date;
 
 /* @author Tony Tan - s3439530 */
 public class Timeslot {
-	private int startTime;
+	private Date startTime;
 	private Date apptDate;
 	
-	Timeslot(int a_startTime, Date a_apptDate) {
+	Timeslot(Date a_startTime, Date a_apptDate) {
 		startTime = a_startTime;
 		apptDate = a_apptDate;
 	}
 	
-	public int getStartTime() {
+	public Date getStartTime() {
 		return startTime;
 	}
 	
