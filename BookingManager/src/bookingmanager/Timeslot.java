8cb014c45e9ff079637817c0292525c3b500c26a
udonge <lolicreamed@gmail.com>
2017-03-29 14:53:42 +1100

    Add files via upload

---
+++ BookingManager/src/bookingmanager/Timeslot.java
@@ -0,0 +1,20 @@
+import java.util.Date;
+
+/* @author Tony Tan - s3439530 */
+public class Timeslot {
+	private int startTime;
+	private Date apptDate;
+	
+	Timeslot(int startTime, Date apptDate) {
+		startTime = a_startTime;
+		apptDate = a_apptDate;
+	}
+	
+	public int getStartTime() {
+		return startTime;
+	}
+	
+	public Date getDate() {
+		return apptDate;
+	}
+}
