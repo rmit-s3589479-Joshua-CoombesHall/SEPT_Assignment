9e9f591b62346be91d2f54a70dcdaf7196590e57
udonge <lolicreamed@gmail.com>
2017-03-31 11:00:20 +1100

    Update Timeslot.java
    /* # Added employeeID 10:59am - 31/03/2017 - Tony */

--- BookingManager/src/bookingmanager/Timeslot.java
+++ BookingManager/src/bookingmanager/Timeslot.java
@@ -2,14 +2,21 @@ package bookingmanager;
 import java.util.Date;
 
 /* @author Tony Tan - s3439530 */
+/* # Added employeeID 10:59am - 31/03/2017 - Tony */
 public class Timeslot {
 	private Date apptDate;
+	private int employeeID;
 	
-	Timeslot(Date a_apptDate) {
+	Timeslot(Date a_apptDate, int a_employeeID) {
 		apptDate = a_apptDate;
+		employeeID = a_employeeID;
 	}
 
 	public Date getDate() {
 		return apptDate;
+	}
+	
+	public int geteID() {
+		return employeeID;
 	}
 }
