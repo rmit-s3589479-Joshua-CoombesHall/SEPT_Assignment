eae28a852ba7963feca3652f577b4bfd3c123e56
Joshua Coombes-Hall <s3589479@student.rmit.edu.au>
2017-04-02 23:45:28 +1000

    Added employee to timeslot

--- BookingManager/src/bookingmanager/Timeslot.java
+++ BookingManager/src/bookingmanager/Timeslot.java
@@ -4,12 +4,19 @@ import java.util.Date;
 /* @author Tony Tan - s3439530 */
 public class Timeslot {
 	private Date apptDate;
+        private Employee assignedEmployee;
 	
-	Timeslot(Date a_apptDate) {
+	Timeslot(Date a_apptDate, Employee a_employee) 
+        {
 		apptDate = a_apptDate;
+                assignedEmployee = a_employee;
 	}
 
 	public Date getDate() {
 		return apptDate;
+	}
+        
+        public Employee getEmployee() {
+		return assignedEmployee;
 	}
 }
