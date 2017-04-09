708194981564950f5ae4d1b4328151b48bfcccfd
Joshua Coombes-Hall <s3589479@student.rmit.edu.au>
2017-04-09 22:10:47 +1000

    Updated change to stop crashing if file doesn't exist.

--- BookingManager/src/bookingmanager/ReadFile.java
+++ BookingManager/src/bookingmanager/ReadFile.java
@@ -18,23 +18,27 @@ public class ReadFile {
 	private static final Type CUSTOMER_TYPE = new TypeToken<List<Customer>>() {}.getType();
 	private static final Type BUSINESS_TYPE = new TypeToken<List<Business>>() {}.getType();
 	
-	public List<Customer> loadCustomers() {
+	public List<Customer> loadCustomers() 
+        {
 		List<Customer> customers = new ArrayList<>();
 		
 		File file = new File("customerDatabase.json");
-		System.out.println("# Loading Customers #");
-		try {
-			Gson gson = new Gson();
-			JsonReader reader = new JsonReader(new FileReader(file));		
-			customers = gson.fromJson(reader, CUSTOMER_TYPE);
-			
-			for(int i = 0 ; i < customers.size(); i ++ ) {
-				System.out.println(customers.get(i).getFirstName());
-			}
-			System.out.println(customers);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
+                if(file.exists())
+                {
+                    System.out.println("# Loading Customers #");
+                    try {
+                            Gson gson = new Gson();
+                            JsonReader reader = new JsonReader(new FileReader(file));		
+                            customers = gson.fromJson(reader, CUSTOMER_TYPE);
+
+                            for(int i = 0 ; i < customers.size(); i ++ ) {
+                                    System.out.println(customers.get(i).getFirstName());
+                            }
+                            System.out.println(customers);
+                    } catch (FileNotFoundException e) {
+                            e.printStackTrace();
+                    }
+                }
 		
 		return customers;	
 	}
@@ -43,19 +47,22 @@ public class ReadFile {
 		List<Business> businesses = new ArrayList<>();
 		
 		File file = new File("businessDatabase.json");
-		System.out.println("# Loading Businesses #");
-		try {
-			Gson gson = new Gson();
-			JsonReader reader = new JsonReader(new FileReader(file));		
-			businesses = gson.fromJson(reader, BUSINESS_TYPE);
-			
-			for(int i = 0 ; i < businesses.size(); i ++ ) {
-				System.out.println(businesses.get(i).getName());
-			}
-			System.out.println(businesses);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}		
+                if(file.exists())
+                {
+                    System.out.println("# Loading Businesses #");
+                    try {
+                            Gson gson = new Gson();
+                            JsonReader reader = new JsonReader(new FileReader(file));		
+                            businesses = gson.fromJson(reader, BUSINESS_TYPE);
+
+                            for(int i = 0 ; i < businesses.size(); i ++ ) {
+                                    System.out.println(businesses.get(i).getName());
+                            }
+                            System.out.println(businesses);
+                    } catch (FileNotFoundException e) {
+                            e.printStackTrace();
+                    }	
+                }
 		
 		return businesses;
 	}
